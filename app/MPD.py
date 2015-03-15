1fed0269976e21aff243cf9cc88bcabdde374165
Superchausette <superchausette@superchausette.fr>
2015-03-15 22:50:58 +0100

    Add code to restart MPD

--- app/MPD.py
+++ app/MPD.py
@@ -1,4 +1,5 @@
 import time
+import subprocess
 
 class MPD:
   def __init__(self):
@@ -10,5 +11,15 @@ class MPD:
       err = "Waiting for at least 15 s since last restart, only %d s" %(currentTime - self.lastRestart)
       return False, err
 
+    cmd = ["service", "mpd", "restart"]
+    p = subprocess.Popen(cmd, stdout = subprocess.PIPE,
+                              stderr =subprocess.PIPE,
+                              stdin  = subprocess.PIPE)
+
+    out, err = p.communicate()
+    rc = p.returncode
+    if rc != 0:
+        err = "Error during restart out:\'%s\' err:\'%s\'" %(out,err)
+        return False, err
     self.lastRestart = time.time()
     return True, "MDP restarted"
